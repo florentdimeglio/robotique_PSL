c4d33d5a3373bcc10ed4d9b9bb02460fbfb0ad3e
Florent Di Meglio <florent.di_meglio@mines-paristech.fr>
2023-06-08 09:12:27 +0200

    Revert "Update VelocityControl.ino"
    This reverts commit 71f6c24a05dd8afb57fa59d245f588adf8f25a8d.

--- code/VelocityControl/VelocityControl.ino
+++ code/VelocityControl/VelocityControl.ino
@@ -3,12 +3,18 @@
   Note: this code requires the following libraries (install them through the library manager):
      - SparkFun I2C Mux Arduino Library
      - AS5600 library
+     - MecatroUtils library (install *.zip file)
+
+  TO FILL:
+      - PI controller parameters Kp and Ki
+      - reference values for the rotational velocities wR and wL
+      - set the right multiplexer ports for the encoders in both the controlLoop() and setup() functions
 */
 
 // Include the current library
 #include "MecatroUtils.h"
 
-// Include the AS5600 library (for the encoders) and Sparkfun I2C Mux (for muliplexer)
+// Include the AS5600 library (for the encoders) and Sparkfun I2C Mux (for multiplexer)
 #include "AS5600.h"
 #include "SparkFun_I2C_Mux_Arduino_Library.h"
 
@@ -17,10 +23,10 @@
 
 // Define the control loop period, in ms.
 #define CONTROL_LOOP_PERIOD 5
-
-//Controller parameters
-float Kp = 0.1;
-float Ki = 1;
+#define BAUD_RATE 1000000
+//Controller parameters ** MUST FILL IN WITH YOUR PARAMETERS
+float Kp = 0;
+float Ki = 0;
 
 // Initialise the integrators
 float IR = 0;
@@ -28,7 +34,7 @@ float IL = 0;
 int time = 0;
 float dt;
 float uR, uL;
-QWIICMUX muliplexer;
+QWIICMUX multiplexer;
 AS5600 rightEncoder, leftEncoder;
 
 // This function is called periodically, every CONTROL_LOOP_PERIOD ms.
@@ -38,19 +44,19 @@ void mecatro::controlLoop()
   dt = (millis() - time) * 1e-3;
   time = millis();
   
-  // Set muliplexer to use port 0 to talk to right encoder.
-  muliplexer.setPort(7);
+  // Set multiplexer to use port 7 to talk to right encoder. ** MUST SET THE RIGHT MULTIPLEXER PORT **
+  multiplexer.setPort(7);
   // Raw encoder measurement - from 0 to 360 degrees
   float wR = rightEncoder.getAngularSpeed(AS5600_MODE_RADIANS);
 
-  // Set muliplexer to use port 3 to talk to left encoder.
-   muliplexer.setPort(4);
+  // Set multiplexer to use port 4 to talk to left encoder. ** MUST SET THE RIGHT MULTIPLEXER PORT **
+   multiplexer.setPort(4);
   // Raw encoder measurement - from 0 to 360 degrees
   float wL = leftEncoder.getAngularSpeed(AS5600_MODE_RADIANS);
   
   //Reference value for angular velocities (rad/s)
-  float wRref = -20;
-  float wLref = -20;
+  float wRref = 0;
+  float wLref = 0;
 
   //Error variables
   float eR = (wR - wRref);
@@ -59,14 +65,13 @@ void mecatro::controlLoop()
   IR += dt * eR;
   IL += dt * eL;
   //Control inputs
-  uR =  - Kp * eR - Ki * IR;
-  uL =  - Kp * eL - Ki * IL;
+  uR = - Kp * eR - Ki * IR;
+  uL = - Kp * eL - Ki * IL;
 
   // Keep the motor off, i.e. at 0 duty cycle (1 is full forward, -1 full reverse)
-  mecatro::setMotorDutyCycle(-uL, -uR);
+  mecatro::setMotorDutyCycle(uL, uR);
 
   mecatro::log("wR",wR);
-  mecatro::log("wL",wL);
 
   // Serial.print("Right speed ");
   // Serial.println(wR);
@@ -78,30 +83,30 @@ void mecatro::controlLoop()
 void setup()
 {
   // Setup serial communication with the PC - for debugging and logging.
-  Serial.begin(1000000);
+  Serial.begin(BAUD_RATE);
   // Start I2C communication
   Wire.begin();
   // Set I2C clock speed to 400kHz (fast mode)
   Wire.setClock(400000);
 
-  // Init muliplexer
-  if (!muliplexer.begin())
+  // Init multiplexer
+  if (!multiplexer.begin())
   {
-    Serial.println("Error: I2C muliplexer not found. Check wiring.");
+    Serial.println("Error: I2C multiplexer not found. Check wiring.");
   }
   else
   {
     bool isInit = true;
-    // Set muliplexer to use port 0 to talk to right encoder.
-    muliplexer.setPort(7);
+    // Set multiplexer to use port 0 to talk to right encoder. ** MUST SET THE RIGHT MULTIPLEXER PORT **
+    multiplexer.setPort(7);
     rightEncoder.begin();
     if (!rightEncoder.isConnected())
     {
       Serial.println("Error: could not connect to right encoder. Check wiring.");
       isInit = false;
     }
-    // Set muliplexer to use port 3 to talk to left encoder.
-    muliplexer.setPort(4);
+    // Set multiplexer to use port 3 to talk to left encoder. ** MUST SET THE RIGHT MULTIPLEXER PORT **
+    multiplexer.setPort(4);
     leftEncoder.begin();
     if (!leftEncoder.isConnected())
     {
